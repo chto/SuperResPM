349be386a4c91b0fba56c435b2c20615a066ae5a
chto <chunhaoto@gmail.com>
2025-04-20 15:55:50 -0500

    remove

--- SuperResPM/test.sh
+++
@@ -1,3 +0,0 @@
-echo "apple banana apple" | awk '{ print gensub(/apple/, "orange", "g", $0) }'
-
-echo "cpu=512,mem=875G,node=10,billing=512" |  awk '{ print int(gensub(/.*cpu=([0-9]+).*/, "\\1", "g", $0)) }' 
